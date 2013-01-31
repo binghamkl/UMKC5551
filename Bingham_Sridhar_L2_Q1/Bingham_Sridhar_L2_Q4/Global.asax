c12ee30fe9203b3e3e0e8a0b3ec02c640678f520
kbinghamibs <bingham.kendall@gmail.com>
2013-01-30 22:35:49 -0600

    Q4 Project

---
+++ Bingham_Sridhar_L2_Q1/Bingham_Sridhar_L2_Q4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bingham_Sridhar_L2_Q4.Global" Language="C#" %>
---
+++ Bingham_Sridhar_L2_Q1/Bingham_Sridhar_L2_Q4/Location.cs
@@ -0,0 +1,87 @@
+﻿#region -- using declarations --
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+#endregion
+
+namespace Bingham_Sridhar_L2_Q4
+{
+    [Serializable()]
+    [XmlRoot("Table")]
+    public class Location
+    {
+
+        private String _city;
+        private String _state;
+        private String _zip;
+        private String _area_Code;
+        private String _time_Zone;
+
+        /*-- Constructors --*/
+
+        #region -- Constructor() --
+        public Location()
+        {
+
+        }
+        #endregion
+
+        /*-- Events --*/
+
+        /*-- Properties --*/
+
+        #region -- City Property --
+        [XmlElement("CITY")]
+        public String City
+        {
+            get { return _city; }
+            set { _city = value; }
+        }
+        #endregion
+
+        #region -- State Property --
+        [XmlElement("STATE")]
+        public String State
+        {
+            get { return _state; }
+            set { _state = value; }
+        }
+        #endregion
+
+        #region -- Zip Property --
+        [XmlElement("ZIP")]
+        public String Zip
+        {
+            get { return _zip; }
+            set { _zip = value; }
+        }
+        #endregion
+
+        #region -- Area_Code Property --
+        [XmlElement("AREA_CODE")]
+        public String Area_Code
+        {
+            get { return _area_Code; }
+            set { _area_Code = value; }
+        }
+        #endregion
+
+        #region -- Time_Zone Property --
+        [XmlElement("TIME_ZONE")]
+        public String Time_Zone
+        {
+            get { return _time_Zone; }
+            set { _time_Zone = value; }
+        }
+        #endregion
+
+        /*-- Methods --*/
+
+        /*-- Event Handlers --*/
+	
+    }
+}
