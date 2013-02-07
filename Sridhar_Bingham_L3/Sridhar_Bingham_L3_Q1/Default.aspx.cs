494ce420a089544a5fdf52c00cf570984492ec29
kbinghamibs <bingham.kendall@gmail.com>
2013-02-06 21:45:18 -0600

    Creation and beginning of Lab 3

---
+++ Sridhar_Bingham_L3/Sridhar_Bingham_L3_Q1/Default.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        String configString = ConfigurationManager.ConnectionStrings["Lab3"].ConnectionString;
+        SqlConnection connection = new SqlConnection(configString);
+        connection.Open();
+
+        SqlCommand cmd = new SqlCommand("Select * from UserAccount", connection);
+        SqlDataReader reader =  cmd.ExecuteReader();
+    }
+}
