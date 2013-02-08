19691a156cfc6080bec5d54dff2a8408d30faa17
unknown <KBingham@KBINGHAM.krygerglass.com>
2013-02-07 23:32:38 -0600

    Started on Unit test.

---
+++ Sridhar_Bingham_L3/Sridhar_Bingham_L3_Q2/MagazineSubscription.cs
@@ -0,0 +1,15 @@
+﻿#region -- using declarations --
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+#endregion
+
+namespace Sridhar_Bingham_L3_Q2
+{
+    public class MagazineSubscription
+    {
+    }
+}
---
+++ Sridhar_Bingham_L3/Sridhar_Bingham_L3_Q2/MagazineSubscriptionFixture.cs
@@ -0,0 +1,29 @@
+﻿#region -- using declarations --
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+#endregion
+
+namespace Sridhar_Bingham_L3_Q2
+{
+    [TestFixture()]
+    public class MagazineSubscriptionFixture
+    {
+
+
+        #region -- TestCreationOfMagazineSubscription() Method --
+        [Test()]
+        public void TestCreationOfMagazineSubscription()
+        {
+            MagazineSubscription magazineSubscription = new MagazineSubscription();
+            Assert.IsNotNull(magazineSubscription, "Did not create magazine subscription option");
+        }
+        #endregion
+	
+
+    }
+}
