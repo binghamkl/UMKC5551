240e1f2d52e856811c7285f92831acafbbf18efa
unknown <KBingham@KBINGHAM.krygerglass.com>
2013-02-09 17:12:32 -0600

    Added in Q2.

--- Sridhar_Bingham_L3/Sridhar_Bingham_L3_Q2/MagazineSubscription.cs
+++ Sridhar_Bingham_L3/Sridhar_Bingham_L3_Q2/MagazineSubscription.cs
@@ -11,5 +11,122 @@ namespace Sridhar_Bingham_L3_Q2
 {
     public class MagazineSubscription
     {
+
+        private String _first;
+        private String _last;
+        private String _address;
+        private String _city;
+        private String _state;
+        private String _zipCode;
+        private String _magazine;
+
+        /*-- Constructors --*/
+
+        #region -- Constructor() --
+        public MagazineSubscription()
+        {
+
+        }
+        #endregion
+
+        /*-- Events --*/
+
+        /*-- Properties --*/
+
+        #region -- First Property --
+        public String First
+        {
+            get { return _first; }
+            set { _first = value; }
+        }
+        #endregion
+
+        #region -- Last Property --
+        public String Last
+        {
+            get { return _last; }
+            set { _last = value; }
+        }
+        #endregion
+
+        #region -- Address Property --
+        public String Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+        #endregion
+
+        #region -- City Property --
+        public String City
+        {
+            get { return _city; }
+            set { _city = value; }
+        }
+        #endregion
+
+        #region -- State Property --
+        public String State
+        {
+            get { return _state; }
+            set { _state = value; }
+        }
+        #endregion
+
+        #region -- ZipCode Property --
+        public String ZipCode
+        {
+            get { return _zipCode; }
+            set { _zipCode = value; }
+        }
+        #endregion
+
+        #region -- Magazine Property --
+        public String Magazine
+        {
+            get { return _magazine; }
+            set { _magazine = value; }
+        }
+        #endregion
+
+        /*-- Methods --*/
+        
+		#region -- SaveSubscription() Method --
+		public int Save()
+		{
+            // Do some saving here.  Get the resulting item back.
+            return 1;
+		}
+		#endregion
+		
+        /*-- Event Handlers --*/
+	
+        /*-- Factory Methods --*/
+        
+		#region -- LoadMagazineSubscription() Method --
+		public static MagazineSubscription LoadMagazineSubscription(int SubscriptionId)
+		{
+            // Fake loading.  Add in loading later.
+            if (SubscriptionId == 1)
+            {
+                MagazineSubscription magazineSubscription = new MagazineSubscription();
+                magazineSubscription.First = "Vidya";
+                magazineSubscription.Last = "Sridhar";
+                magazineSubscription.Address = "Address";
+                magazineSubscription.City = "City";
+                magazineSubscription.State = "State";
+                magazineSubscription.ZipCode = "64106";
+                magazineSubscription.Magazine = "Time";
+
+                return magazineSubscription;
+            }
+            else
+            {
+                return null;
+            }
+		}
+		#endregion
+		
+    
     }
 }
--- Sridhar_Bingham_L3/Sridhar_Bingham_L3_Q2/MagazineSubscriptionFixture.cs
+++ Sridhar_Bingham_L3/Sridhar_Bingham_L3_Q2/MagazineSubscriptionFixture.cs
@@ -23,6 +23,47 @@ namespace Sridhar_Bingham_L3_Q2
             Assert.IsNotNull(magazineSubscription, "Did not create magazine subscription option");
         }
         #endregion
+
+        #region -- MagazineSubscriptionAcceptsNameAddressInformation() Method --
+        [Test()]
+        public void MagazineSubscriptionAcceptsNameAddressInformation()
+        {
+            MagazineSubscription magazineSubscription = new MagazineSubscription();
+            magazineSubscription.First = "Vidya";
+            magazineSubscription.Last = "Sridhar";
+            magazineSubscription.Address = "Address";
+            magazineSubscription.City = "City";
+            magazineSubscription.State = "State";
+            magazineSubscription.ZipCode = "64106";
+
+            Assert.AreEqual("Vidya", magazineSubscription.First, "First Name must have set/get");
+            Assert.AreEqual("Sridhar", magazineSubscription.Last, "Last Name must have set/get");
+            Assert.AreEqual("Address", magazineSubscription.Address, "Address must have setter/getter");
+            Assert.AreEqual("City", magazineSubscription.City, "City must have setter/getter");
+            Assert.AreEqual("State", magazineSubscription.State, "State must have setter/getter");
+            Assert.AreEqual("64106", magazineSubscription.ZipCode, "ZipCode must have setter/getter");
+
+        }
+        #endregion
+
+        #region -- SubmitMagazineSubscriptionSavesInformationIntoSubscriptions() Method --
+        [Test()]
+        public void SubmitMagazineSubscriptionSavesInformationIntoSubscriptions()
+        {
+            MagazineSubscription magazineSubscription = new MagazineSubscription();
+            magazineSubscription.First = "Vidya";
+            magazineSubscription.Last = "Sridhar";
+            magazineSubscription.Address = "Address";
+            magazineSubscription.City = "City";
+            magazineSubscription.State = "State";
+            magazineSubscription.ZipCode = "64106";
+            magazineSubscription.Magazine = "Time";
+            int subScriptionId = magazineSubscription.Save();
+
+            MagazineSubscription savedSubscription = MagazineSubscription.LoadMagazineSubscription(subScriptionId);
+            Assert.IsNotNull(savedSubscription, "Did not load a saved subscription");
+        }
+        #endregion
 	
 
     }
